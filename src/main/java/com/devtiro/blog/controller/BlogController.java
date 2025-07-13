5c30aa0aa1534474e27d7a6cef911848a514f13e
Romit Sutariya <romitsutariya@gmail.com>
2025-07-13 09:44:37 +0530

    rest endpoint to check the status of application

---
+++ src/main/java/com/devtiro/blog/controller/BlogController.java
@@ -0,0 +1,14 @@
+package com.devtiro.blog.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class BlogController {
+    @GetMapping(path = {"/check"})
+    public String check(){
+        return "Application is up and running...!";
+    }
+}
