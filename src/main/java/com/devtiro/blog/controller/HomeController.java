c6ae93bd8afb0b9199fe249eac8ae9e093112f84
Romit Sutariya <romitsutariya@gmail.com>
2025-07-13 13:21:31 +0530

    add new Home controller get mapping

--- src/main/java/com/devtiro/blog/controller/HomeController.java
+++ src/main/java/com/devtiro/blog/controller/HomeController.java
@@ -1,9 +1,11 @@
 package com.devtiro.blog.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+     @GetMapping
      public  String home(){
          return "<H1>Blog Application</h1>";
      }
