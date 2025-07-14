9e1c83dca203b6a530e7ee80b0b9ea897604fb2c
Romit Sutariya <romitsutariya@gmail.com>
2025-07-14 07:50:03 +0530

    show hostname in index page

--- src/main/java/com/devtiro/blog/controller/HomeController.java
+++ src/main/java/com/devtiro/blog/controller/HomeController.java
@@ -1,12 +1,22 @@
 package com.devtiro.blog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @Controller
 public class HomeController {
      @GetMapping("/")
-     public  String home(){
+     public  String home(Model model){
+         try {
+             String hostname = InetAddress.getLocalHost().getHostName();
+             model.addAttribute("hostname", hostname);
+         } catch (UnknownHostException e) {
+             model.addAttribute("hostname", "Unknown Host");
+         }
          return "index";
      }
 }
